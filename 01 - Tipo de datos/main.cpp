a2a07f6ac3d335974c2120349b04c03d9751b04e
JuanPabloGalgano <102485023+JuanPabloGalgano@users.noreply.github.com>
2022-05-16 11:57:22 -0300

    Add files via upload

---
+++ 01 - Tipo de datos/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <string>
+
+
+int main() {
+        assert ((true and false) == false);
+        assert ((25*4 == 100) != 500/10);
+        assert ('a' + 13 == 'n');
+        assert (177.8 >= 177);
+        assert ("Hola mundo" != "hello world");
+
+
+
+
+}
